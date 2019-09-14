94dec323e2e68a181d0471fa4dcc8d80589bd584
chency92 <185215187@qq.com>
2019-09-14 12:48:00 +0800

    test

--- 调试转换格式/接口格式转换.py
+++
@@ -1,9 +0,0 @@
-def add_open():
-    r = str(input("接口路径："))
-    url_path = "/api/open/" + r.split("api/")[1]
-    print(url_path)
-
-
-if __name__ == '__main__':
-    while True:
-        add_open()
